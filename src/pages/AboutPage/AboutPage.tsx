d6e18dccdf6663db9976094ede1ce77c9a3ab942
Arayik77 <arayik_grigoryan_97@mail.ru>
2024-04-04 23:14:55 +0400

    project start

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">About Page</h1>
+        <p className="mt-2">
+          This is the about page. You can put some information about your
+          application here.
+        </p>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
