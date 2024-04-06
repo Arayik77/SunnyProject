a3be3bf5115db7f7ade3a7888a2a45e2e75fe008
Arayik77 <arayik_grigoryan_97@mail.ru>
2024-04-06 18:14:37 +0400

    mode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,43 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import AboutPageAsync from "@/pages/AboutPage/AboutPage.async";
 import MainPageAsync from "@/pages/MainPage/MainPage.async";
 import { Link, Route, Routes } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 const App = () => {
+    const [mode, setMode] = useState(false); // true - dark , false - light
+
+    useEffect(() => {
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            setMode(true);
+        } else {
+            setMode(false);
+        }
+    },[]);
+
+    const handleButtonClick = () => {
+        setMode(chanegMode => !chanegMode);
+    };
+
+    const contentStyle = {
+        backgroundColor: mode ? '#000' : '#fff',
+        color: mode ? '#fff' : '#000'
+      };
+
 
     return (
-        <div className="flex flex-col h-screen">
-            <nav className="bg-slate-200">
+        <div style={contentStyle} className="flex flex-col h-screen">
+            <nav style={contentStyle} className="bg-slate-200">
                 <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-bettween h-16">
                         <div className="flex">
-                            <Link 
+                            <Link style={contentStyle}
                                 to="/" 
                                 className="text-zinc-600 px-3 py-2 rounded-md text-md font-semibold"
                             >
                                 Main
                             </Link>
-                            <Link 
+                            <Link style={contentStyle}
                                 to="/about" 
                                 className="text-zinc-600 px-3 py-2 rounded-md text-md font-semibold"
                             >
@@ -30,7 +49,13 @@ const App = () => {
             </nav>
 
             <div>
-                <Button className="absolute top-0 right-0 m-2 p-2">Change Theme Mode</Button>
+                <Button
+                    onClick={handleButtonClick}
+                    className="absolute top-0 right-0 m-2 p-2"
+                    variant={mode ? 'destructive' : 'secondary'}
+                >
+                    {mode ? 'Dark Mode' : 'Light Mode'}
+                </Button>
             </div>
 
             <div className="flex-grow py-5">
