3fc909ecbd53d267adaf7b01511555b6862cad05
Arayik77 <arayik_grigoryan_97@mail.ru>
2024-04-20 15:53:43 +0400

    rigth bar

--- src/components/shared/RightSidebar/RightSidebar.tsx
+++ src/components/shared/RightSidebar/RightSidebar.tsx
@@ -1,144 +1,155 @@
-import { Link } from "react-router-dom"
-import image from "@/components/images/rightside/image.webp";
-import { LucideArrowRight } from "lucide-react";
+import RenderTag from "@/components/shared/cards/RenderTag";
+import QuestionSmallCard from "@/components/shared/cards/QuestionSmallCard";
+import PostSmallCard from "@/components/shared/cards/PostSmallCard";
 
-const topPosts = [
-    {
-        id: 1,
-        img: image,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
-    {
-        id: 2,
-        img: image,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
-    {
-        id: 3,
-        img: image,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
-    {
-        id: 4,
-        img: image,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
-    {
-        id: 5,
-        img: image,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
+const topPosts: any = [
+  {
+    id: 1,
+    title:
+      "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
+    image:
+      "https://www.plusdev.net/_next/image?url=https%3A%2F%2Fplusdev-net.s3.eu-central-1.amazonaws.com%2Fbd1c1480201129be867adf88effb1458_nodejs-smart-contracts.jpg&w=640&q=60",
+    url: "/posts/test",
+  },
+  {
+    id: 2,
+    title:
+      "Asynchronous Programming in Node.js: Callbacks, Promises, and Async/Await Explained",
+    image:
+      "https://www.plusdev.net/_next/image?url=https%3A%2F%2Fplusdev-net.s3.eu-central-1.amazonaws.com%2Fbd1c1480201129be867adf88effb1458_nodejs-smart-contracts.jpg&w=640&q=60",
+    url: "/posts/test",
+  },
+  {
+    id: 3,
+    title:
+      "Node.js vs. Golang: An In-Depth Comparison of the Latest Versions in 2024",
+    image:
+      "https://www.plusdev.net/_next/image?url=https%3A%2F%2Fplusdev-net.s3.eu-central-1.amazonaws.com%2Fbd1c1480201129be867adf88effb1458_nodejs-smart-contracts.jpg&w=640&q=60",
+    url: "/posts/test",
+  },
+  {
+    id: 4,
+    title:
+      "Mastering Concurrency in Go: A Comprehensive Guide to Goroutines and Channels",
+    image:
+      "https://www.plusdev.net/_next/image?url=https%3A%2F%2Fplusdev-net.s3.eu-central-1.amazonaws.com%2Fbd1c1480201129be867adf88effb1458_nodejs-smart-contracts.jpg&w=640&q=60",
+    url: "/posts/test",
+  },
 ];
 
-const topQuestions = [
-    {
-        id: 1,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
-    {
-        id: 2,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    },
-    {
-        id: 3,
-        url: "/",
-        text: "Mastering Smart Contract Development with Node.js: Tools, Security, and Best Practices",
-    }
+const hotQuestions: any = [
+  {
+    id: 1,
+    title:
+      "Is there a way in Golang to define a generic function type with varargs that can take any function as input?",
+    url: "/questions/test",
+  },
+  {
+    id: 2,
+    title: "How get access the unmodified body of a Fastify request",
+    url: "/questions/test",
+  },
+  {
+    id: 3,
+    title: "Unable to download generated file using node js to pc",
+    url: "/questions/test",
+  },
 ];
 
-const topTags = [
-    {
-        id: 1,
-        url: "/",
-        title: "NODE JS",
-        quantity: 5,
-    },
-    {
-        id: 2,
-        url: "/",
-        title: "GO (GOLANG)",
-        quantity: 19,
-    },
-    {
-        id: 3,
-        url: "/",
-        title: "FASTIFY",
-        quantity: 1,
-    },
-    {
-        id: 4,
-        url: "/",
-        title: "NODE JS",
-        quantity: 40,
-    },
-    {
-        id: 5,
-        url: "/",
-        title: "GO",
-        quantity: 6,
-    }
+const popularTags: any = [
+  {
+    id: 1,
+    title: "Node.js",
+    count: 10,
+    url: "/tags/test",
+  },
+  {
+    id: 2,
+    title: "Vue.js",
+    count: 20,
+    url: "/tags/test",
+  },
+  {
+    id: 3,
+    title: "Angular",
+    count: 1,
+    url: "/tags/test",
+  },
+  {
+    id: 4,
+    title: "React",
+    count: 13,
+    url: "/tags/test",
+  },
+  {
+    id: 5,
+    title: "Golang",
+    count: 8,
+    url: "/tags/test",
+  },
 ];
 
 const RightSidebar = () => {
-    return (
-        <section className="w-[350px] custom-scrollbar sticky right-0 top-0 flex h-screen flex-col justify-between overflow-y-auto 
-        shadow-light-300
-        border-l border-zinc-200 bg-zinc-50 p-6 pt-36 max-xl:hidden lg:w-[350px] dark:border-zinc-900 dark:bg-zinc-900">
-            <div>
-                <h3 className="h3-bold text-zinc-900 dark:text-zinc-200">Top posts</h3>
-                <div className="flex flex-col mt-5 w-full gap-[30px]">
-                    {topPosts?.map((item) => {
-                        return (
-                            <Link to={item.url} className="flex gap-3 items-center justify-between" key={item.id}>
-                                <img src={item.img} alt={item.img} className="h-[50px] w-[50px] rounded-[10px] object-cover" />
-                                <span className="small-text text-zinc-700 hover:text-red-500 dark:text-zinc-400">{item.text}</span>
-                                <span><LucideArrowRight className="w-5 h-5"/></span>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </div>
+  return (
+    <section
+      className="custom-scrollbar sticky right-0 top-0 flex
+    h-screen w-[350px] flex-col overflow-y-auto border-l
+    border-zinc-200 bg-zinc-50 dark:border-zinc-900 dark:bg-zinc-900
+    p-6 pt-36 max-xl:hidden"
+    >
+      <div>
+        <h3 className="h3-bold text-zinc-900 dark:text-zinc-200">Top Posts</h3>
 
-            <div className="mt-16">
-                <h3 className="h3-bold text-zinc-900 dark:text-zinc-200">Top Questions</h3>
-                <div className="flex flex-col mt-5 w-full gap-[30px]">
-                    {topQuestions?.map((item) => {
-                        return (
-                            <Link to={item.url} className="flex gap-5 items-center justify-between" key={item.id}>
-                                <span className="small-text text-zinc-700 hover:text-red-500 dark:text-zinc-400">{item.text}</span>
-                                <span><LucideArrowRight className="w-5 h-5"/></span>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </div>
+        <div className="mt-5 flex w-full flex-col gap-[30px]">
+          {topPosts.map((post) => (
+            <PostSmallCard
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              url={post.url}
+              image={post.image}
+            />
+          ))}
+        </div>
+      </div>
 
-            <div className="mt-16">
-                <h3 className="h3-bold text-zinc-900 dark:text-zinc-200">Top Tags</h3>
-                <div className="flex flex-col mt-5 w-full gap-[15px]">
-                    {topTags?.map((item) => {
-                        return (
-                            <Link to={item.url} className="flex gap-2 items-center justify-between" key={item.id}>
-                                <div className="bg-slate-300 px-4 py-2 uppercase text-zinc-500 hover:bg-indigo-400
-                                    hover:bg-indigo-400 hover:text-zinc-100 dark:bg-slate-800 
-                                    dark:text-zinc-100 hover:dark:bg-indigo-400 items-center border text-xs rounded-md">
-                                    {item.title}
-                                </div>
-                                <span className="small-medium text-zinc-700 dark:text-zinc-400">{item.quantity}</span>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </div>
-        </section>
-    )
-}
+      <div className="mt-16">
+        <h3 className="h3-bold text-zinc-900 dark:text-zinc-200">
+          Top Questions
+        </h3>
 
-export default RightSidebar
+        <div className="mt-5 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map((question) => (
+            <QuestionSmallCard
+              key={question.id}
+              id={question.id}
+              title={question.title}
+              url={question.url}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <h3 className="h3-bold text-zinc-900 dark:text-zinc-200">
+          Popular Tags
+        </h3>
+
+        <div className="mt-3 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              id={tag.id}
+              title={tag.title}
+              url={tag.url}
+              key={tag.id}
+              showCount={true}
+              totalCount={tag.count}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
